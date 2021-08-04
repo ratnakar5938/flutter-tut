202eb4b548549ba7723d23cd8cbf64a6980eb85c
Ratnakar Sahoo <ratnakar3895@yahoo.com>
2021-08-05 00:19:31 +0530

    icons

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,11 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.deepPurpleAccent,
       ),
       body: Center(
-        // child: Image(
-          // image: NetworkImage('https://wallpapershome.com/images/pages/ico_v/21906.jpg'),
-          // image: AssetImage('assets/1.jpg'),
-        // ),
-        child: Image.asset('assets/2.jpg'), // short hand
-        // child: Image.network('https://wallpaperaccess.com/full/1213672.jpg'),
+        child: Icon(
+          Icons.airport_shuttle,
+          color: Colors.lightBlue,
+          size: 50,
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Text("click"),
